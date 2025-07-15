b90ac4d6281b85869d3a58cbdb9212562edd7dcd
NataliyaStavtseva <stav.tasha@gmail.com>
2025-07-15 11:38:19 +0300

    added 404.html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
   mode: 'production',
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: '/otus_weather_forecast/',
+    publicPath: '/otus_weather_forecast',
   },
   devtool: 'source-map',
   devServer: {
@@ -35,7 +35,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
